da7f22b0a045a05dfdc0998b6fb5242e5e4fefa0
marlon <marzonhapacheco@gmail.com>
2024-06-21 17:32:36 -0300

    Sidebar & Post

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,7 +1,32 @@
+import styles from './post.module.css'
+
 export default function Post() {
   return (
     <>
-    
+      <article className={styles.post}>
+        {/* Header apenas está tendo função semantica */}
+        <header>
+          <div className={styles.author}>
+            <img src="src\assets\avatar.png" alt="" />
+            <div className={styles.authorInfo}>
+              <strong>Feliphe Araújo dos Santos</strong>
+              <span>Desenvolvedor</span>
+            </div>
+          </div>
+        </header>
+        <div className={styles.content}>
+          <p>Sobre você</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non error optio culpa iure maiores totam, praesentium exercitationem suscipit, sint quasi aspernatur magni velit, ipsum sequi molestiae aliquid nostrum accusantium vero?</p>
+
+          <p><a>Link Repositórios</a></p>
+          <a>Último Projeto</a>
+        </div>
+
+        <form action="">
+          <strong>Experiência profissional</strong>
+        </form>
+
+      </article>
     </>
     
   )
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,19 @@
+import styles from './sidebar.module.css'
+
 export default function Sidebar(){
     return(
         <>
-            
+            <aside className={styles.sidebar}>
+                <img className={styles.cover} src="src\assets\banner.jpg" alt="" />
+                <div className={styles.profile}>
+                    <img className={styles.avatar} src="src\assets\avatar.png" alt="" />
+                    <strong>Feliphe Araújo dos Santos</strong>
+                    <span>Programador FullStack</span>
+                </div>
+                <footer>
+                    <a href="">Github</a>
+                </footer>
+            </aside>
         </>
     )
 }
